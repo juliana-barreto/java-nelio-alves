85374881d19b6390c587dfb8f818b00e58e259c3
Juliana <89608026+juliana-barreto@users.noreply.github.com>
2025-08-05 18:37:29 -0300

    add gender attribute to Person

--- src/main/java/io/github/juliana_barreto/entities/Person.java
+++ src/main/java/io/github/juliana_barreto/entities/Person.java
@@ -5,8 +5,9 @@ public class Person {
   private final String name;
   private final int age;
   private final double height;
+  private final char gender;
 
-  public Person(String name, int age, double height) {
+  public Person(String name, int age, double height, char gender) {
     if (name == null || name.isEmpty()) {
       throw new IllegalArgumentException("Name cannot be null or empty.");
     }
@@ -16,9 +17,21 @@ public class Person {
     if (height < 0) {
       throw new IllegalArgumentException("Height cannot be negative.");
     }
+    if (gender != 'F' && gender != 'M' && gender != 'X') {
+      throw new IllegalArgumentException("Gender must be 'F' or 'M' or 'X' for unspecified.");
+    }
     this.name = name;
     this.age = age;
     this.height = height;
+    this.gender = gender;
+  }
+
+  public Person(String name, int age, double height) {
+    this(name, age, height, 'X');
+  }
+
+  public Person(double height, char gender) {
+    this("N/A", 0, height, gender);
   }
 
   public String getName() {
@@ -31,5 +44,9 @@ public class Person {
 
   public double getHeight() {
     return height;
+  }
+
+  public char getGender() {
+    return gender;
   }
 }
